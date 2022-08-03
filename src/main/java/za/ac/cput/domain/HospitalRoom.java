11c33443d66092a7d2b8aaaee0b539f781a3c3a4
Fayaad Abrahams <fayaad1999@gmail.com>
2022-08-03 22:57:35 +0200

    First Commit
    Added Entities + Repositories for ->
     * Nurse
     * Hospital Room

---
+++ src/main/java/za/ac/cput/domain/HospitalRoom.java
@@ -0,0 +1,35 @@
+package za.ac.cput.domain;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+    HospitalRoom.java
+    Entity for the Hospital Rooms
+    Author: Fayaad Abrahams (218221630)
+    Date: 3 August 2022
+*/
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@Table(name = "tbl_hospital_room")
+public class HospitalRoom {
+
+    @Id
+    private String roomID;
+
+    @NotNull
+    private int roomFloor;
+
+    public HospitalRoom(String roomID, int roomFloor) {
+        this.roomID = roomID;
+        this.roomFloor = roomFloor;
+    }
+}
---
+++ src/main/java/za/ac/cput/domain/Nurse.java
@@ -0,0 +1,36 @@
+package za.ac.cput.domain;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+    Nurse.java
+    Entity for the Nurses
+    Author: Fayaad Abrahams (218221630)
+    Date: 3 August 2022
+*/
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@Table(name = "tbl_nurse")
+public class Nurse {
+
+    @Id
+    private String nurseID;
+    @NotNull
+    private String nurseFirstName;
+    private String nurseLastName;
+
+    public Nurse(String nurseID, String nurseFirstName, String nurseLastName) {
+        this.nurseID = nurseID;
+        this.nurseFirstName = nurseFirstName;
+        this.nurseLastName = nurseLastName;
+    }
+}
